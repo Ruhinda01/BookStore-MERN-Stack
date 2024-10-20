535c8b193f8c3d35485a0a01a9dfb49e9cd98365
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-10-21 01:25:46 +0300

    Worked on basic UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router } from 'react-router-dom'
 import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import EditBook from './pages/EditBook';
@@ -9,13 +10,15 @@ import ShowBook from './pages/ShowBook';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/books/create" element={<CreateBooks />} />
-      <Route path="/books/details/:id" element={<ShowBook />} />
-      <Route path="/books/edit/:id" element={<EditBook />} />
-      <Route path="/books/delete/:id" element={<DeleteBook />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books/create" element={<CreateBooks />} />
+        <Route path="/books/details/:id" element={<ShowBook />} />
+        <Route path="/books/edit/:id" element={<EditBook />} />
+        <Route path="/books/delete/:id" element={<DeleteBook />} />
+      </Routes>
+    </Router>
   )
 };
 
---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,8 @@
+
+const Spinner = () => {
+  return (
+    <div className="animate-spin rounded-full w-16 h-16 m-8 bg-sky-600"></div>
+  )
+}
+
+export default Spinner
